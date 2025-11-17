732f78eb52b6c01b663b4fe29886a7bb60177912
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-11-17 11:58:54 +0100

    app added

--- src/app.py
+++ src/app.py
@@ -1,1 +1,14 @@
 import chainlit as cl
+from src.loader import order_request, messages
+
+
+@cl.on_message 
+async def main(message: cl.Message):
+    messages.append({"role": "user", "content": message.content})
+    response = order_request(messages)
+    messages.append({"role": "assistant", "content": response})
+
+    # Send message back to the user
+    await cl.Message(
+        content=response,
+    ).send()
