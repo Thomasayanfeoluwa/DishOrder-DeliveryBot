ea876fe1207fd4ba20a24ac3d5b2f085bd9d00e6
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2025-11-17 11:49:49 +0100

    loader for llm

---
+++ src/loader.py
@@ -0,0 +1,19 @@
+from groq import Groq
+from src.prompt import system_instruction
+
+
+client = Groq()
+
+# Initialize LLM
+messages = [
+    {"role": "system", "content": system_instruction}
+]
+
+
+def order_request(messages, model = "llama-3.3-70b-versatile", temperature = 0):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature
+    )
+    return response.choices[0].message.content
