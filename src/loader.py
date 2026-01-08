6d830b74ce555f5b544d5298c09b6266227ac67c
Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-01-08 20:51:23 +0100

    Use Streamlit secrets for API key loading
    Replaced loading of API key from environment variable with Streamlit secrets management.

--- src/loader.py
+++ src/loader.py
@@ -1,11 +1,16 @@
 from groq import Groq
 from src.prompt import system_instruction
+import streamlit as st
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.environ.get("GROQ_API_KEY")
+# api_key = os.environ.get("GROQ_API_KEY")
+api_key = st.secrets.get("GROQ_API_KEY")
+
+
+st.write("API key loaded:", "Yes" if api_key else "No")
 
 if not api_key:
     raise ValueError("GROQ_API_KEY environment variable not set.")
